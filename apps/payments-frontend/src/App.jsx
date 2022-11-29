2ccecf28fd7f2351fb61375adf4ace5c3871b546
anna274 <anna_rusakovich.01@mail.ru>
2022-11-29 15:00:24 +0300

    feat: add comment

--- apps/payments-frontend/src/App.jsx
+++ apps/payments-frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 // here you can get platform helper
 import { getQDPTitle } from 'utils';
+// here you can get vendor helper
 // import { getQDPTitle } from '0be4f8de-647f-11eb-ae93-0242ac130002';
 
 const App = () => {
