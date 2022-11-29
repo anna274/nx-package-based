1bf5afbe1c0e86b7e99cd6a36f04837abfbb644b
anna274 <anna_rusakovich.01@mail.ru>
2022-11-29 14:40:02 +0300

    feat: add payments-frontend app

---
+++ apps/payments-frontend/src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+// here you can get platform helper
+import { getQDPTitle } from 'utils';
+// import { getQDPTitle } from '0be4f8de-647f-11eb-ae93-0242ac130002';
+
+const App = () => {
+  return <h1>{getQDPTitle()}</h1>;
+};
+
+export default App;
